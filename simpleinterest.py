a013864378092bc0213edc9eba8e1f71606e8e71
Hemalatha G V <164108712+Hemalatha-gv@users.noreply.github.com>
2024-11-17 20:36:05 -0600

    Create simpleinterest.py

---
+++ simpleinterest.py
@@ -0,0 +1,7 @@
+principal=float(input("enter the principal amount"))
+rate_of_interest=float(input("enter the rate of interest"))
+time_period=int(input("Enter time period"))
+
+simple_interest= principal * rate_of_interest_time_period
+
+print(simple_interest)
